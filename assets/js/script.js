ce69f2b1f8c195e731dd17fcfda5b7e23a6d3c3b
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-20 17:22:20 -0400

    added event listener to js file

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let apiKey = 'd9e8fa6428a49966fe0b0aa58e369bf7'
+let cityNameVal = document.querySelector("input").value
+
+var submitButton = document.querySelector(".btn");
+
+submitButton.addEventListener("click", function(){
+   console.log("button clicked");
+});
