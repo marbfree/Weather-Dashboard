7f36c045b0cb680d6a3488f838a87ffe4723dbb6
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-20 19:51:06 -0400

    working on adding fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
 let apiKey = 'd9e8fa6428a49966fe0b0aa58e369bf7'
 let cityNameVal = document.querySelector("input").value
-
 var submitButton = document.querySelector(".btn");
 
-submitButton.addEventListener("click", function(){
-   console.log("button clicked");
-});
+function getApi(){
+    let requestUrl = "http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid=d9e8fa6428a49966fe0b0aa58e369bf7"
+
+    fetch(requestUrl)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data);
+        });  
+};
+
+
+submitButton.addEventListener("click", getApi);
+   
