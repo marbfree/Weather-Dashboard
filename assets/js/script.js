7569e2ec737ec69937a97f50bd52783769f833fd
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-20 20:16:23 -0400

    printing example api

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,16 @@ let cityNameVal = document.querySelector("input").value
 var submitButton = document.querySelector(".btn");
 
 function getApi(){
-    let requestUrl = "http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid=d9e8fa6428a49966fe0b0aa58e369bf7"
+    // let requestUrl = "http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit=1&appid=d9e8fa6428a49966fe0b0aa58e369bf7"
+        let requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Atlanta&limit=1&appid=d9e8fa6428a49966fe0b0aa58e369bf7"
 
     fetch(requestUrl)
         .then(function (response){
             return response.json();
         })
         .then(function (data){
-            console.log(data);
+            for (let i = 0; i < data.length; i++){
+            console.log(data[i].lat);}
         });  
 };
 
