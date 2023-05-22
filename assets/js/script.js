466c2a28e0f710f04fe933facdb573403c910d49
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-22 12:15:06 -0400

    add city name to url

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,43 +8,35 @@ function getApi(){
     // set value to local storage
     let getCity = localStorage.setItem("cityName", cityNameVal);
     console.log(getCity);
-    
-
-    // get value from local storage to plug into url
-        // relplace search with city name
-    
-  function searchApi(){
-        
-    // seperate city from limit in query parameters
-    let cityArr = document.location.search.split('&');
-    console.log(cityArr);
-    // replace city name with city name from input value
-    let addCity = cityArr[0].replace(cityNameVal);
-    console.log(addCity);
+    searchApi(cityNameVal);
 }
 
-    searchApi();
-    console.log(searchApi);
+//    get latitude and longitude from api call
+    
+  function searchApi(cityNameVal){
+        console.log(cityNameVal);  
 
-    let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=d9e8fa6428a49966fe0b0aa58e369bf7`
-
+    let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=` + cityNameVal + `&limit=1&appid=d9e8fa6428a49966fe0b0aa58e369bf7`
+    let requestUrlLatLon = `http://api.openweathermap.org/geo/1.0/reverse?` +lat={lat}&lon={lon} + `&limit=1&appid=d9e8fa6428a49966fe0b0aa58e369bf7`
 
     fetch(requestUrl)
         .then(function (response){
             return response.json();
         })
         .then(function (data){
+            console.log(data);
             for (let i = 0; i < data.length; i++){
               let cityDaily = document.querySelector("#currentCity");
               let lonDaily = document.querySelector("#currentLongitude");
               let latDaily = document.querySelector("#currentLatitude");
               cityDaily.textContent = data[i].name;
-              lonDaily.textContent = data[i].coord;
-              latDaily.textContent = data[i].main;
+              lonDaily.textContent = data[i].lon;
+              latDaily.textContent = data[i].lat;
             console.log(data[i]);
             console.log(data[i]);}
         });  
-};
+
+    }
 
 
 submitButton.addEventListener("click", getApi);
