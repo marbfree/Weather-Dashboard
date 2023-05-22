e830cd5560a0e48ed6d48f1254b30522db1da67b
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-22 10:08:09 -0400

    set input value to local Storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,53 @@
 let apiKey = 'd9e8fa6428a49966fe0b0aa58e369bf7'
-let cityNameVal = document.querySelector("input").value
+// let cityNameVal = document.querySelector("input").value
+// console.log(cityNameVal);
 var submitButton = document.querySelector(".btn");
 
-// add function to get input (city) to add to api
+// add function to get input to add to api
+
+
 
 function getApi(){
-    // let requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},&limit=1&appid=d9e8fa6428a49966fe0b0aa58e369bf7"
-        let requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Atlanta&limit=1&appid=d9e8fa6428a49966fe0b0aa58e369bf7"
-        // let requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=d9e8fa6428a49966fe0b0aa58e369bf7"
+    let cityNameVal = document.querySelector("input").value;
+    console.log(cityNameVal);
 
+   let getCity = localStorage.setItem("cityName", cityNameVal);
+    console.log(getCity);
+
+    // let cityArr = document.location.search.split('&');
+    // console.log(cityArr);
+
+    // let city = cityArr[0].split('=').pop();
+
+    // searchApi(city);
+    // console.log(searchApi);
+
+function searchApi(city){
+
+        let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=d9e8fa6428a49966fe0b0aa58e369bf7`
+        
     fetch(requestUrl)
         .then(function (response){
             return response.json();
         })
         .then(function (data){
             for (let i = 0; i < data.length; i++){
-              let cityDaily = document.createElement("div");
-              cityDaily.textContent = data[i].city;
+              let cityDaily = document.querySelector("#currentCity");
+              let lonDaily = document.querySelector("#currentLongitude");
+              let latDaily = document.querySelector("#currentLatitude");
+              cityDaily.textContent = data[i].name;
+              lonDaily.textContent = data[i].coord;
+              latDaily.textContent = data[i].main;
             console.log(data[i]);
             console.log(data[i]);}
         });  
 };
-
-// function getApi(){
-//     // let requestUrl = "http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit=1&appid=d9e8fa6428a49966fe0b0aa58e369bf7"
-//         let requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Atlanta&limit=1&appid=d9e8fa6428a49966fe0b0aa58e369bf7"
-
-//     fetch(requestUrl)
-//         .then(function (response){
-//             return response.json();
-//         })
-//         .then(function (data){
-//             for (let i = 0; i < data.length; i++){
-//             console.log(data[i].lat);
-//             console.log(data[i].lon);}
-//         });  
-// };
-
+}
 
 submitButton.addEventListener("click", getApi);
-   
+
+
+// get value from input
+// pass value to url
+// get lat/long from query
+// resubmit using lat/long to get 5 day forcast
